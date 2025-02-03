6ab0c4b1115f4d90343804fd4020360dc10251c3
AshishAd <ashish10adhikari@gmail.com>
2025-02-03 16:42:22 +0545

    added bottom navbar

--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -6,10 +6,11 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
+    <>
     <div className="grid grid-cols-1 md:grid-cols-4">
       <div className="grid grid-cols-1 md:grid-cols-3 col-span-3 md:flex-wrap">
         <div className="col-span-2 flex justify-center md:justify-end items-center md:py-5 md:pr-2">
-          <img src={logo} alt="Logo" className="md:w-40 md:h-20 w-20 h-10"/>
+          <img src={logo} alt="Logo" className="md:w-44 md:h-20 w-20 h-10"/>
           <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold font-primary text-slate-800">
             Aayushma Motorboat  
           </h1>
@@ -32,6 +33,19 @@ const Navbar = () => {
         </ul>
       </div>
     </div>
+    <div className="container w-full bg-slate-800 text-white text-center py-2">
+        <ul className="flex justify-center gap-5 h-14 items-center">
+          <li>Home</li>
+          <li>Tours/Services</li>
+          <li>Our Boats</li>
+          <li>Gallery</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li><button className="bg-sky-700 p-3 rounded-md">Book Now</button></li>
+        </ul>
+    </div>
+    </>
+    
   );
 };
 
