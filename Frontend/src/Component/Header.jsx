7fa74c107b7493a722f93875f7f2404d20d402fd
AshishAd <ashish10adhikari@gmail.com>
2025-02-04 12:35:53 +0545

    customized navbar & header

--- Frontend/src/Component/Header.jsx
+++ Frontend/src/Component/Header.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import hero from '../assets/hero.jpg';
 const Header = () => {
   return (
-    <div style={{backgroundImage: `url(${hero})`}} className="bg-cover bg-center h-[75vh] flex items-center justify-center">
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold font-primary text-white text-center">
-            Welcome to Aayushma Motorboat
-        </h1>
+    <div style={{backgroundImage: `url(${hero})`}} className="bg-cover bg-center h-[75vh] flex items-center justify-center text-white font-bold">
+      <div className='text-center'>
+        <h2 className='text-3xl'>Welcome To</h2>
+        <h1 className='text-5xl'>Aayushma Motorboat Adventure</h1>
+        <div className='gap-5 flex justify-center'>
+          <button className='bg-cyan-700 py-5 px-7'>Package</button>
+            <button className='bg-cyan-700 py-5 px-7'>Book Now</button>
+        </div>
+      </div>
+        
     </div>
   )
 }
--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -7,45 +7,74 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
     <>
-    <div className="grid grid-cols-1 md:grid-cols-4">
-      <div className="grid grid-cols-1 md:grid-cols-3 col-span-3 md:flex-wrap">
-        <div className="col-span-2 flex justify-center md:justify-end items-center md:py-5 md:pr-2">
-          <img src={logo} alt="Logo" className="md:w-44 md:h-20 w-20 h-10"/>
-          <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold font-primary text-slate-800">
-            Aayushma Motorboat  
-          </h1>
+      <div className="grid grid-cols-1 md:grid-cols-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 col-span-3 md:flex-wrap">
+          <div className="col-span-2 flex justify-center md:justify-end items-center md:py-5 md:pr-2">
+            <img src={logo} alt="Logo" className="md:w-44 md:h-20 w-20 h-10" />
+            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold font-primary text-slate-800 whitespace-nowrap">
+              Aayushma Motorboat
+            </h1>
+          </div>
+          <h2 className="text-gray-600 text-sm font-medium col-span-1 flex items-center justify-center md:justify-start md:pt-5">
+            Conquering the waves
+          </h2>
         </div>
-        <h2 className="text-gray-600 text-sm font-medium col-span-1 flex items-center justify-center md:justify-start md:pt-5">
-          Conquering the waves
-        </h2>
+        <div className="grid col-span-1">
+          <div className="flex justify-center items-center gap-1 py-2">
+            <FontAwesomeIcon icon={faPhone} className="text-gray-600"/>
+          <span className="text-gray-600 text-sm font-medium"><strong className="pr-2">Phone:</strong>+977 9800000000</span>
+          </div>
+          
+        </div>
       </div>
-      <div className="grid col-span-1">
-        <ul className="flex justify-center gap-5 md:p-5 items-center">
-          <li>
-            <FontAwesomeIcon icon={faFacebook} style={{ color: "#1742ee" }} />
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faPhone} />
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faTiktok} style={{ color: "#080a0d" }} />
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div className="container w-full bg-slate-800 text-white text-center py-2">
+      <div className=" w-full bg-slate-800 text-white text-center py-2">
         <ul className="flex justify-center gap-5 h-14 items-center">
-          <li>Home</li>
-          <li>Tours/Services</li>
-          <li>Our Boats</li>
-          <li>Gallery</li>
-          <li>About us</li>
-          <li>Contact us</li>
-          <li><button className="bg-sky-700 p-3 rounded-md">Book Now</button></li>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            HOME
+          </a>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            PACKAGE
+          </a>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            OUR BOATS
+          </a>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            GALLERY
+          </a>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            ABOUT US
+          </a>
+          <span className="border-1 h-6"></span>
+          <a
+            href="#"
+            className="hover:bg-cyan-700 font-secondary font-bold py-5 px-6 transition-all ease-in-out duration-300"
+          >
+            CONTACT US
+          </a>
+          <span className="border-1 h-6"></span>
         </ul>
-    </div>
+      </div>
     </>
-    
   );
 };
 
