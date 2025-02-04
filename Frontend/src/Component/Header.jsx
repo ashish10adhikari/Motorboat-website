ec3ff5e673cbf5c69ff15c879c08fe4ef1cd8180
AshishAd <ashish10adhikari@gmail.com>
2025-02-04 09:33:05 +0545

    added favicon

--- Frontend/src/Component/Header.jsx
+++ Frontend/src/Component/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import hero from '../assets/hero.jpg';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div style={{backgroundImage: `url(${hero})`}} className="bg-cover bg-center h-[75vh] flex items-center justify-center">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold font-primary text-white text-center">
+            Welcome to Aayushma Motorboat
+        </h1>
+    </div>
   )
 }
 
