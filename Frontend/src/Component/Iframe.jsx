0fffdac2b09c17ebb0e5ba7d6f0bc8403d7e4471
AshishAd <ashish10adhikari@gmail.com>
2025-02-06 21:56:59 +0545

    updated topnavbar &iframe

--- Frontend/src/Component/Iframe.jsx
+++ Frontend/src/Component/Iframe.jsx
@@ -5,7 +5,7 @@ const Iframe = () => {
     <div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.5669699709447!2d84.41973557532347!3d27.69977567618689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3994fbc1ed4ddca7%3A0x289c1bb28da049e0!2sAyushma%20Motorboat%20Station!5e0!3m2!1sen!2snp!4v1738854748780!5m2!1sen!2snp"
-       className="w-full h-64"
+       className="w-full h-96"
       ></iframe>
     </div>
   );
--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -1,31 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 col-span-3 md:flex-wrap">
-          <div className="col-span-2 flex justify-center md:justify-end items-center md:py-5 md:pr-2">
-            <img src={logo} alt="Logo" className="md:w-44 md:h-20 w-20 h-10" />
-            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold font-primary text-slate-800">
-              Aayushma Motorboat
-            </h1>
-          </div>
-          <h2 className="text-gray-600 text-sm font-medium col-span-1 flex items-center justify-center md:justify-start md:pt-5">
-            Conquering the waves
-          </h2>
-        </div>
-        <div className="grid col-span-1">
-          <div className="flex justify-center items-center gap-1 py-2 md:text-lg">
-            <FontAwesomeIcon icon={faPhone} className="text-gray-500"/>
-          <span className="text-sky-600"><span className="pr-2 md:font-semibold">Phone:</span>+977 9800000000</span>
-          </div>
-          
-        </div>
+      <div className="flex flex-col md:flex-row items-center justify-center p-5">
+        <img src={logo} alt="Logo" className="md:w-44 md:h-20 w-20 h-10" />
+        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold font-primary text-slate-800">
+          Aayushma Motorboat
+        </h1>
+        <h2 className="text-gray-600 text-sm font-medium flex justify-center md:justify-start md:pt-5 md:pl-2">
+          Conquering the waves
+        </h2>
       </div>
+
       <div className=" w-full bg-slate-800 text-white text-center py-2">
         <ul className="flex justify-center gap-5 h-14 items-center">
           <span className="border-1 h-6"></span>
