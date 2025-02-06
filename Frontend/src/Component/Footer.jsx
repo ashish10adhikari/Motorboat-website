45cc1ed12748682e95ae7949ceb51aa0bc3d1f77
AshishAd <ashish10adhikari@gmail.com>
2025-02-06 20:58:49 +0545

    completed footer.jsx

--- Frontend/src/Component/Footer.jsx
+++ Frontend/src/Component/Footer.jsx
@@ -1,9 +1,275 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTiktok,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faLocationDot,
+  faEnvelope,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div>
+      <footer className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 pb-6 pt-16 sm:px-6 lg:px-8 lg:pt-24">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+            <div>
+              <div className="flex justify-center text-teal-600 sm:justify-start">
+                <img src={logo} alt="" className="w-44 h-20" />
+              </div>
+
+              <p className=" max-w-md text-center leading-relaxed text-gray-500 sm:max-w-xs sm:text-left text-3xl font-primary">
+                Aayushma Motorboat
+              </p>
+
+              <ul className="mt-8 flex justify-center gap-6 sm:justify-start md:gap-8">
+                <li>
+                  <a
+                    href="#"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-teal-700 transition hover:text-teal-700/75"
+                  >
+                    <FontAwesomeIcon icon={faFacebook} className="text-xl" />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-teal-700 transition hover:text-teal-700/75"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} className="text-xl" />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-teal-700 transition hover:text-teal-700/75"
+                  >
+                    <FontAwesomeIcon icon={faTiktok} className="text-xl" />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-teal-700 transition hover:text-teal-700/75"
+                  >
+                    <FontAwesomeIcon icon={faWhatsapp} className="text-xl" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4 lg:col-span-2">
+              <div className="text-center sm:text-left">
+                <p className="text-lg font-medium text-gray-900">Quick Link</p>
+
+                <ul className="mt-8 space-y-4 text-sm">
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Package
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Our Boats
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Gallery
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      {" "}
+                      About Us{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="text-center sm:text-left">
+                <p className="text-lg font-medium text-gray-900">
+                  Our Services
+                </p>
+
+                <ul className="mt-8 space-y-4 text-sm">
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Sunset Cruises
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      {" "}
+                      Private Charter Services{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      {" "}
+                      River Tours{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      {" "}
+                      Adventure Rides{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="text-center sm:text-left">
+                <p className="text-lg font-medium text-gray-900">
+                  Helpful Links
+                </p>
+
+                <ul className="mt-8 space-y-4 text-sm">
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      FAQs
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Terms & Conditions
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-700 transition hover:text-gray-700/75"
+                      href="#"
+                    >
+                      Safety Guidelines
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="text-center sm:text-left">
+                <p className="text-lg font-medium text-gray-900">Contact Us</p>
+
+                <ul className="mt-8 space-y-4 text-sm">
+                  <li>
+                    <a
+                      className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                      href="#"
+                    >
+                      <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className="text-teal-700 transition hover:text-teal-700/75"
+                      />
+                      <span className="flex-1 text-gray-700">
+                        aayushmamotorboat@gmail.com
+                      </span>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                      href="#"
+                    >
+                      <FontAwesomeIcon
+                        icon={faPhone}
+                        className="text-teal-700 transition hover:text-teal-700/75"
+                      />
+                      <span className="flex-1 text-gray-700">0123456789</span>
+                    </a>
+                  </li>
+
+                  <li className="flex items-start justify-center gap-1.5 sm:justify-start">
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      className="text-teal-700 transition hover:text-teal-700/75"
+                    />
+
+                    <address className="-mt-0.5 flex-1 not-italic text-gray-700">
+                      Narayani River, Gate-1, Narayanghat
+                    </address>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-12 border-t border-gray-100 pt-6">
+            <div className="text-center sm:flex sm:justify-center sm:text-left">
+              <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
+                &copy; {currentYear} Aayushma Motorboat Company Pvt. Ltd.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
