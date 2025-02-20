8d6e952fb75816fb28bca1de440ca22b8c086616
AshishAd <ashish10adhikari@gmail.com>
2025-02-20 14:13:07 +0545

    updated login page

--- Frontend/src/Admin/Login.jsx
+++ Frontend/src/Admin/Login.jsx
@@ -4,15 +4,14 @@ import logo from "../assets/logo.png";
 const Login = () => {
   return (
     <div className="flex items-center justify-center text-center min-h-screen">
-      <div className="min-w-lg">
-        <div className="flex flex-row text-center items-center justify-center">
-          <img src={logo} alt="" className="w-36 h-20" />
-          <h1 className="text-2xl font-primary text-gray-700">
-            Aayushma River Adventure
-          </h1>
-        </div>
+      
         <form action="" className="bg-gray-100 p-10 rounded-2xl mt-2">
-          <h2 className="text-xl font-bold text-gray-700">Login</h2>
+          <div className="flex flex-row text-center items-center justify-center">
+            <img src={logo} alt="" className="w-36 h-20" />
+            <h1 className="text-2xl font-primary text-gray-700">
+              Aayushma River Adventure
+            </h1>
+          </div>
           <div className="flex flex-col items-start pt-5">
             <label htmlFor="email">Your Username</label>
             <input
@@ -31,14 +30,23 @@ const Login = () => {
           </div>
           <div className="p-5 flex justify-between items-center">
             <div className="flex gap-2 justify-center items-center">
-              <input type="checkbox" name="rememberme" id="" className="w-4 h-4"/>
+              <input
+                type="checkbox"
+                name="rememberme"
+                id=""
+                className="w-4 h-4"
+              />
               <label htmlFor="rememberme">Remember Me</label>
             </div>
-            <a href="" className="text-blue-600 font-medium">Forgot Password?</a>
+            <a href="" className="text-blue-600 font-medium">
+              Forgot Password?
+            </a>
           </div>
-          <button className="bg-cyan-700 w-full text-white p-3 border-2 border-white hover:bg-white hover:text-cyan-700 hover:border-cyan-700 hover:scale-105 transition-all duration-500">LOGIN</button>
+          <button className="bg-cyan-700 w-full text-white p-3 border-2 border-white hover:bg-white hover:text-cyan-700 hover:border-cyan-700 hover:scale-105 transition-all duration-500">
+            LOGIN
+          </button>
         </form>
-      </div>
+      
     </div>
   );
 };
