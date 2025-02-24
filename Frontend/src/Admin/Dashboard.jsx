bb34eb1ff620689d5beb2612d621e2ebfa7f6c7e
AshishAd <ashish10adhikari@gmail.com>
2025-02-24 22:27:23 +0545

    added simple dashboard

--- Frontend/src/Admin/Dashboard.jsx
+++ Frontend/src/Admin/Dashboard.jsx
@@ -3,8 +3,24 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
+    <div>
+      <h1 className='font-secondary text-3xl p-10'>Dashboard</h1>
+      <div className='grid grid-cols-3 gap-5'>
+        <div className='w-sm bg-cyan-700 p-5 rounded-lg text-white col-span-1'>
+          <h2>10</h2>
+          <h3>Messages</h3>
+        </div>
+        <div className='w-sm bg-cyan-700 p-5 rounded-lg text-white col-span-1'>
+          <h2>10</h2>
+          <h3>Images</h3>
+        </div>
+        <div className='w-sm bg-cyan-700 p-5 rounded-lg text-white col-span-1'>
+          <h2>10</h2>
+          <h3>Boats</h3>
+        </div>
+      </div>
+    </div>
     
-    <h1>Dashboard</h1>
   )
 }
 
