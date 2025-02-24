94668d5cc704ec2b7a9cf7df82e93c302c851be9
AshishAd <ashish10adhikari@gmail.com>
2025-02-24 12:03:41 +0545

    added packagecontroller

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('package', [PackageController::class, 'package'])->name('package');
