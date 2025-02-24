8bfca639b1d1966c51bf93e548806c777fb461b0
AshishAd <ashish10adhikari@gmail.com>
2025-02-24 20:29:17 +0545

    minor css changes

--- Frontend/src/Layout/DashboardLayout.jsx
+++ Frontend/src/Layout/DashboardLayout.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Topbar from '../Component/Topbar'
-import Adminsidebar from '../Component/Adminsidebar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Topbar from "../Component/Topbar";
+import Adminsidebar from "../Component/Adminsidebar";
 
 const DashboardLayout = () => {
   return (
-    <>
-    <Topbar/>
-    <Adminsidebar/>
-    <Outlet/>
-    </>
-  )
-}
+    <div className="flex flex-col h-screen">
+      <Topbar />
 
-export default DashboardLayout
+      <div className="flex flex-1">
+        <Adminsidebar />
+
+        <div className="flex-1 p-4 ml-60">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
