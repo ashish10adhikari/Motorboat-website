2c39f1c69817c26d5a7e34283f6bf5b093897ca2
AshishAd <ashish10adhikari@gmail.com>
2025-02-25 13:07:19 +0545

    completed form for package

--- Frontend/src/Admin/Component/AdminPackage.jsx
+++ Frontend/src/Admin/Component/AdminPackage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../Component/adminpackage.css'
 
 const AdminPackage = () => {
   return (
@@ -6,24 +7,72 @@ const AdminPackage = () => {
       <div className="flex flex-col text-center">
         <h1 className="text-3xl font-secondary">Packages</h1>
 
-        <form action="" className="flex flex-row w-full justify-center items-center">
-          <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" />
+        <form
+          action=""
+          className="flex flex-col w-full justify-center items-center gap-5"
+        >
+          <div className="flex flex-row gap-5">
+            <div className="gap-2 flex ">
+              <label
+                htmlFor="title"
+                className="justify-center items-center flex"
+              >
+                Title:
+              </label>
+              <input
+                type="text"
+                name="title"
+                className="p-3 border border-gray-700 focus:outline-none focus:ring focus:ring-cyan-700 focus:border-cyan-700"
+              />
+            </div>
+            <div className="gap-2 flex ">
+              <label
+                htmlFor="price"
+                className="justify-center items-center flex"
+              >
+                Price:
+              </label>
+              <input
+                type="number"
+                name="price"
+                className="p-3 border border-gray-700 focus:outline-none focus:ring focus:ring-cyan-700 focus:border-cyan-700"
+              />
+            </div>
+
+            <div className="gap-2 flex ">
+              <label
+                htmlFor="image"
+                className="justify-center items-center flex"
+              >
+                Image:
+              </label>
+              <input
+                type="file"
+                name="image"
+                accept="image/png, image/jpeg"
+                id="image"
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" />
+
+          <div className="flex flex-row gap-5">
+            <div className="gap-2 flex ">
+              <label
+                htmlFor="description"
+                className="justify-center items-center flex"
+              >
+                Description:
+              </label>
+              <input
+                type="text"
+                name="description"
+                className="p-3 border border-gray-700 focus:outline-none focus:ring focus:ring-cyan-700 focus:border-cyan-700"
+              />
+            </div>
+            <button className="bg-cyan-700 py-3 px-6 border-2 border-white hover:bg-white hover:text-cyan-700 hover:border-cyan-700 hover:scale-105 transition-all duration-500 text-white">
+              Add
+            </button>
           </div>
-          <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" />
-          </div>
-          <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" />
-          </div>
-          <button>Add</button>
         </form>
       </div>
     </div>
