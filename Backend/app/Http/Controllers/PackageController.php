1c6f855492a600e67cfd6900abc67ec10c545d64
AshishAd <ashish10adhikari@gmail.com>
2025-02-27 20:45:47 +0545

    added function for package api

--- Backend/app/Http/Controllers/PackageController.php
+++ Backend/app/Http/Controllers/PackageController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Package;
 
 class PackageController extends Controller
 {
-    // public class package(){
-
-    // }
+    public function package(){
+        $package = Package::all();
+        $data =[
+           " package" => $package,
+            "message" => 200
+        ];
+        return response()->json($data);
+    }
 }
